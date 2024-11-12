3f4704e17a6b2361c0fc9e1d8ec2d94e0a6b3197
altair313 <153547043+altair313@users.noreply.github.com>
2024-11-12 22:32:12 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    apiKey: '5cc260919be32752f3611e4eadd8e949', // Replace with your actual API key
+    baseUrl: 'https://api.themoviedb.org/3',
+    imageBaseUrl: 'https://image.tmdb.org/t/p/w500'
+};
